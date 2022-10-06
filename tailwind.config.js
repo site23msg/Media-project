4362d32a63a35d1e6190eecf0382d2ce6c28a9ea
Marosi Balázs <websupport@agrohof.com>
2022-10-06 13:01:35 +0200

    host

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'Darkblue': '#010E29',
+        'Lightblue':'#006FFF',
+        'Darkgray':'#1A2439D9',
+        'Lightgray':'#2E384A',
+        'Darkgraytoggle':'#f0f0f0f5',
+        'Lightgraytoggle':'#F7F7F7',
+        'divideGray':'#1A2940'
+      },
+      fontFamily: {
+        hummerBold: ["Hurme Geometric Sans-4",'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
