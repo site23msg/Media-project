baa5d253db92ecaede5f0d8f95452ed1d1f6aea2
Marosi Balázs <websupport@agrohof.com>
2022-10-07 11:17:18 +0200

    font family added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./docs/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Darkblue': '#010E29',
+        'Lightblue':'#006FFF',
+        'Darkgray':'#1A2439D9',
+        'Lightgray':'#2E384A',
+        'Darkgraytoggle':'#f0f0f0f5',
+        'Lightgraytoggle':'#F7F7F7',
+        'divideGray':'#1A2940'
+      },
+    },
   },
   plugins: [],
 }
