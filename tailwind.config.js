d769e35242a083e82dcbe0b75846283c380c163f
Marosi Balázs <websupport@agrohof.com>
2022-10-07 13:04:03 +0200

    ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./docs/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
-        'Darkblue': '#010E29',
-        'Lightblue':'#006FFF',
-        'Darkgray':'#1A2439D9',
-        'Lightgray':'#2E384A',
-        'Darkgraytoggle':'#f0f0f0f5',
-        'Lightgraytoggle':'#F7F7F7',
-        'divideGray':'#1A2940'
-      },
-    },
-  },
-  plugins: [],
+    extend: { colors: { 
+      'Darkblue': '#010E29', 
+    'Lightblue':'#006FFF', 
+    'Darkgray':'#1A2439D9', 
+    'Lightgray':'#2E384A', 
+    'Darkgraytoggle':'#f0f0f0f5', 
+    'Lightgraytoggle':'#F7F7F7', 
+    'divideGray':'#1A2940' 
+    }, 
+  }, 
+}, 
+plugins: [], 
 }
