dda144333930c231201fe6963fd003d40e151271
Marosi Balázs <websupport@agrohof.com>
2022-10-13 09:18:51 +0200

    darkMode fixed

--- src/app.js
+++ src/app.js
@@ -1,9 +1,4 @@
 import Alpine from 'alpinejs'
- 
 window.Alpine = Alpine
-Alpine.data()
- 
 Alpine.start();
 
-const agro = document.getElementById('agro');
-console.log(agro)
--- src/custom.js
+++
@@ -1,5 +0,0 @@
-
-const agro = document.getElementById('agro');
-console.log(agro);
-const content = document.getElementById('content');
-console.log(content)
