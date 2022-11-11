38baab8dfdd4862d80f6c871b51ebe2ad85b6308
Galaczi Arnold <sitebuild@23msg.com>
2022-11-11 16:45:38 +0100

    project filter resolved

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 
 let barMask = document.querySelector('.barMask')
-
 const barTrack = document.querySelector('.barTrack')
-
 const toggelBtn = document.querySelectorAll('.toggle')
+const web = document.querySelectorAll('.web')
+const social = document.querySelectorAll('.social')
+const video = document.querySelectorAll('.video')
+const photo = document.querySelectorAll('.photo')
+const graphic = document.querySelectorAll('.graphic')
+const projects = document.querySelectorAll('.project')
+let container = document.querySelector('.container')
 
 let barMaskWidth = barMask.clientWidth/5
 
@@ -21,26 +26,94 @@ toggelBtn.forEach(btn => {
                 step = 0;
                 console.log(step);
                 barTrack.style.transform = "translateX(" + step + "px)";
+                projects.forEach(item => {
+                    item.classList.remove('hidden');
+                })
+                container.classList.add("xl:justify-between")
             break;
             case '2':
                 step = barMaskWidth;
                 console.log(step);
                 barTrack.style.transform = "translateX(" + step + "px)";
+                web.forEach(item => {
+                    item.classList.remove('hidden')
+                })
+                container.classList.remove("xl:justify-between")
+                social.forEach(item =>  {
+                    item.classList.add('hidden')
+                });
+                video.forEach(item =>  {
+                    item.classList.add('hidden')
+                });
+                photo.forEach(item =>  {
+                    item.classList.add('hidden')
+                });
+                graphic.forEach(item =>  {
+                    item.classList.add('hidden')
+                });
             break;
             case '3':
                 step = 2 * barMaskWidth;
                 console.log(step);
                 barTrack.style.transform = "translateX(" + step + "px)";
+                graphic.forEach(item => {
+                    item.classList.remove('hidden')
+                })
+                container.classList.remove("xl:justify-between")
+                social.forEach(item =>  {
+                    item.classList.add('hidden')
+                });
+                video.forEach(item =>  {
+                    item.classList.add('hidden')
+                });
+                photo.forEach(item =>  {
+                    item.classList.add('hidden')
+                });
+                web.forEach(item =>  {
+                    item.classList.add('hidden')
+                });
             break;
             case '4':
                 step = 3 * barMaskWidth;
                 console.log(step);
                 barTrack.style.transform = "translateX(" + step + "px)";
+                video.forEach(item =>  {
+                    item.classList.remove('hidden')
+                });
+                container.classList.remove("xl:justify-between")
+                social.forEach(item =>  {
+                    item.classList.add('hidden')
+                });
+                photo.forEach(item =>  {
+                    item.classList.add('hidden')
+                });
+                web.forEach(item =>  {
+                    item.classList.add('hidden')
+                });
+                graphic.forEach(item =>  {
+                    item.classList.add('hidden')
+                });
             break;
             case '5':
                 step = 4 * barMaskWidth;
                 console.log(step);
                 barTrack.style.transform = "translateX(" + step + "px)";
+                photo.forEach(item =>  {
+                    item.classList.remove('hidden')
+                });
+                container.classList.remove("xl:justify-between")
+                video.forEach(item =>  {
+                    item.classList.remove('hidden')
+                });
+                social.forEach(item =>  {
+                    item.classList.add('hidden')
+                });
+                graphic.forEach(item =>  {
+                    item.classList.add('hidden')
+                });
+                web.forEach(item =>  {
+                    item.classList.add('hidden')
+                });
             break;
         }
     })
